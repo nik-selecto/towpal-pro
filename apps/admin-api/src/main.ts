af6db6b3f5df8c4f24c714907bff3d6ef4b82595
Nikita Moiseienko <nikitamoiseienko@Nikitas-MacBook-Pro.local>
2022-09-14 19:09:11 +0300

    feat(husky): pre-commit - "lint", pre-push - "test"

--- apps/admin-api/src/main.ts
+++ apps/admin-api/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
-  await app.listen(port); 
+  await app.listen(port);
   Logger.log(
     `🚀 NODE_ENV=${process.env.NODE_ENV} Application is running on: http://localhost:${port}/${globalPrefix}`
   );
